867cb80e82fc7807790a2a325d53804ad2106509
julian santos <julian.santos@b2wdigital.com>
2019-06-30 09:52:53 -0300

    desing basico

--- anoum-count/src/App.js
+++ anoum-count/src/App.js
@@ -1,24 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var loadingProgress = 45;
+  var loadingClasse = 1;
+  switch (loadingProgress){
+    default:
+    case loadingProgress<33:
+      loadingClasse = 1;
+    case 33<loadingProgress<66:
+      loadingClasse = 2;
+    case loadingProgress>66:
+      loadingClasse = 3;  
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="full-wrapper">
+      <header className="title"> meu vagao :) 
       </header>
+      <wrapper className="train-wrapper">       
+        <wrapper className="wagon"> 
+          <div className="box"><div className="loading" ></div></div>
+          <div className="wagonDrawInicio"></div>
+        </wrapper>
+
+        <wrapper className="wagon">
+          <div className="box"></div>
+          <img className="wagonDrawMeio"></img>
+        </wrapper>
+
+        <wrapper className="wagon"> 
+          <div className="box"></div>
+          <img className="wagonDrawMeio"></img>
+        </wrapper>
+
+        <wrapper className="wagon"> 
+          <div className="box"></div>
+          <img className="wagonDrawMeio"></img>
+        </wrapper>
+
+        <wrapper className="wagon"> 
+          <div className="box"></div>
+          <img className="wagonDrawMeio"></img>
+        </wrapper>
+
+        <wrapper className="wagon"> 
+          <div className="box"></div>
+          <img className="wagonDrawFim"></img>
+        </wrapper>
+
+      </wrapper>
+
     </div>
   );
 }
